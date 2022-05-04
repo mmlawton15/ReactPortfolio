559c5fce6809aae325f9febc3048994b2cc6ee53
Mary Margaret <mmlawton15@gmail.com>
2022-05-04 09:39:05 -0400

    working on wave code

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import About from './components/pages/About';
 import Portfolio from './components/pages/Portfolio';
 import Resume from './components/pages/Resume';
 import Home from './components/pages/Home';
-
+import Footer from './components/Footer/index.js'
 
 export default function App() {
   const [currentPage, setCurrentPage] = useState('Home');
@@ -28,9 +28,12 @@ export default function App() {
   }
 
   return (
-    <div>
-      <Nav currentPage={currentPage} handlePageChange={setCurrentPage}></Nav>
-      {renderPage()}
-    </div>
+    <main>
+      <div>
+        <Nav currentPage={currentPage} handlePageChange={setCurrentPage}></Nav>
+        {renderPage()}
+      </div>
+      <Footer/>
+    </main>
   );
 }
